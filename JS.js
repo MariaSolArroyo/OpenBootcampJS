c56ecdd4fd479ef5d5d2f9f2e61b5d92951c7563
MariaSolArroyo <103608743+MariaSolArroyo@users.noreply.github.com>
2022-09-27 21:20:38 -0300

    Ejercicio 2

---
+++ JS.js
@@ -0,0 +1,8 @@
+
+
+const datos =["María Sol",34,true,new Date(1988,8,17),libroFav ={
+    titulo:"The subtle art of not giving a f@ck",
+    autor:"Mark Manson",
+    fecha: new Date(2010,5,12),
+    url: "https://www.amazon.com/importe-Diario-subtle-Journal-Spanish/dp/8418870958"}]
+
