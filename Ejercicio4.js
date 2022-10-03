79a60293a4cf2ce030097cf488417f9fe564fac8
MariaSolArroyo <103608743+MariaSolArroyo@users.noreply.github.com>
2022-10-02 21:44:04 -0300

    Ejercicio 4 - Strings

---
+++ Ejercicio4.js
@@ -0,0 +1,19 @@
+let nombre = "María Sol";
+
+let apellido = "Arroyo";
+
+let estudiante = nombre.concat(" ",apellido);
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let numLetras = estudiante.length;
+
+let primerLetra = nombre.charAt(0);
+
+let ultLetra = apellido.charAt(apellido.length-1);
+
+let eliminarEspacios = estudiante.trim();
+
+let contenida = estudiante.includes(nombre);
